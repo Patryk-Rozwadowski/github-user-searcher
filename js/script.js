4f099dfdaacc0d7c33bc7a7ebe60871993130d9b
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-07-15 23:56:52 +0200

    Add styles, onChangeText now searching after 2 intupts

--- js/script.js
+++ js/script.js
@@ -11,16 +11,19 @@ class App extends React.Component {
         this.setState({
             searchText: event.target.value
         })
+        if (this.state.searchText.length > 2) {
+            this.onSubmit();
+        }
     }
 
-    onSubmit(e) {
+    onSubmit() {
         event.preventDefault();
         const url = `https://api.github.com/search/users?q=${this.state.searchText}`
         fetch(url)
             .then(response => response.json())
             .then(responseJson => this.setState({ users: responseJson.items }))
-
     }
+    
 
     render() {
         return (
@@ -33,7 +36,7 @@ class App extends React.Component {
                         placeholder="Search username"
                         onChange={e => this.onChangeText(e)} />
                 </form>
-                <UsersList users={this.state.users} />
+                {Product(users)}
             </div>
         )
     }
@@ -42,11 +45,12 @@ class App extends React.Component {
 class UsersList extends React.Component {
     get users() {
         return this.props.users.map(user => <User key={user.id} user={user} />)
+        
     }
 
     render() {
         return (
-            <div>
+            <div className="users-container">
                 {this.users}
             </div>
         )
@@ -56,12 +60,33 @@ class UsersList extends React.Component {
 class User extends React.Component {
     render() {
         return (
-            <div>
+            <div className="user-container">
                 <img src={this.props.user.avatar_url} />
-                <a href={this.props.user.url}>{this.props.user.login}</a>
+                <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
+                <p>Account type: {this.props.user.type}</p>
             </div>
         )
     }
+}
+
+const Product(props) {
+    let content = [];
+    props.user.forEach((product, i) => {
+        if ((i + 1) % 4 == 0) {
+            content.push(
+                <div className="row" key={product.id}>
+                    <article key={product.id} className="col-md-3"></article>
+                </div>
+            )
+        } else {
+            content.push(<article key={product.id} className="col-md-3"></article>);
+        }
+    });
+    return (
+        <div>
+            {content}
+        </div>
+    );
 }
 
 ReactDOM.render(
