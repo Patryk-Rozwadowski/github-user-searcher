813866fe3bf84a5c173fb29c4a07ba1c4014fb9e
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-07-16 12:11:33 +0200

    Add new styles, add grid layout for users

--- js/script.js
+++ js/script.js
@@ -22,8 +22,8 @@ class App extends React.Component {
         fetch(url)
             .then(response => response.json())
             .then(responseJson => this.setState({ users: responseJson.items }))
+
     }
-    
 
     render() {
         return (
@@ -36,7 +36,7 @@ class App extends React.Component {
                         placeholder="Search username"
                         onChange={e => this.onChangeText(e)} />
                 </form>
-                {Product(users)}
+                <UsersList users={this.state.users}/>
             </div>
         )
     }
@@ -45,7 +45,6 @@ class App extends React.Component {
 class UsersList extends React.Component {
     get users() {
         return this.props.users.map(user => <User key={user.id} user={user} />)
-        
     }
 
     render() {
@@ -61,32 +60,12 @@ class User extends React.Component {
     render() {
         return (
             <div className="user-container">
-                <img src={this.props.user.avatar_url} />
+                <a href={this.props.user.html_url} target="_blank"><img src={this.props.user.avatar_url} /></a>
                 <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
                 <p>Account type: {this.props.user.type}</p>
             </div>
         )
     }
-}
-
-const Product(props) {
-    let content = [];
-    props.user.forEach((product, i) => {
-        if ((i + 1) % 4 == 0) {
-            content.push(
-                <div className="row" key={product.id}>
-                    <article key={product.id} className="col-md-3"></article>
-                </div>
-            )
-        } else {
-            content.push(<article key={product.id} className="col-md-3"></article>);
-        }
-    });
-    return (
-        <div>
-            {content}
-        </div>
-    );
 }
 
 ReactDOM.render(
